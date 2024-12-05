5ff28a1c5e27a4837d35827684599e53d745bff0
iffysravioli <21itmj@gmail.com>
2024-12-05 18:52:35 -0500

    answer upload

--- LSP_Fall_2024/src/org/howard/edu/lsp/finalp1/HelloWorld.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalp1/HelloWorld.java
@@ -1,7 +1,64 @@
 /**
 *  Name: Ifetayo Spencer
 *  
-*  
+*  Section 1: 
+F
+F
+F
+T
+F
+T
+F
+T
+F
+F
+T
+T
+SKIP 13 FULL CREDIT
+F
+F
+F
+F
+F
+F
+F
+
+Section 2: 
+B
+A
+C
+C
+C
+B
+C
+A, C
+B
+B
+B
+B
+B
+D
+B
+A, B
+B, c, f
+A
+B
+D
+C
+A
+C
+B
+D
+A
+B
+C
+B
+B
+
+
+
+
+
 *  
 *  
 *  
